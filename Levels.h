34cca6c7cffefb324af0d928154d3218382d7748
Thomas <thomas.vantroyen@gmail.com>
2019-10-06 23:30:34 +0200

    vault area finished

--- Levels.h
+++ Levels.h
@@ -4,8 +4,8 @@
 namespace levels {
 	// copy the indicated part at the top of the generated code (by the levels importer) to here :
 	constexpr uint8_t newgame_spawnpoint = 0;
-	constexpr uint8_t level_count = 1;
-	constexpr uint8_t trigger_count = 5;
+	constexpr uint8_t level_count = 7;
+	constexpr uint8_t trigger_count = 32;
 	// ----
 
 
