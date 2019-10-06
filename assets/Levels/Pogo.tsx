04a8d702e66c4c2d53274be11178bb0157a65bf6
Thomas <thomas.vantroyen@gmail.com>
2019-10-06 21:30:06 +0200

    Added assets and levels

---
+++ assets/Levels/Pogo.tsx
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Stuck" tilewidth="24" tileheight="32" tilecount="32" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="../walltiles16.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../walltiles-grass16.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="editor_secret_wall.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="../ladder.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="32" source="../door_blue2.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="32" source="../door_green2.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="32" source="../door_orange2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="32" source="../door_red2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="10" height="10" source="../gem_blue.png"/>
+ </tile>
+ <tile id="10">
+  <image width="10" height="10" source="../gem_green.png"/>
+ </tile>
+ <tile id="11">
+  <image width="10" height="10" source="../gem_orange.png"/>
+ </tile>
+ <tile id="12">
+  <image width="10" height="10" source="../gem_red.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="../clue.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="../spikes.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="editor_portal.png"/>
+ </tile>
+ <tile id="26">
+  <image width="20" height="25" source="princess1.png"/>
+ </tile>
+ <tile id="31">
+  <image width="24" height="20" source="../goblin1.png"/>
+ </tile>
+ <tile id="32">
+  <image width="24" height="22" source="../imp-idle1.png"/>
+ </tile>
+ <tile id="34">
+  <image width="16" height="16" source="../potion.png"/>
+ </tile>
+ <tile id="37">
+  <image width="16" height="16" source="stairs_add.png"/>
+ </tile>
+ <tile id="38">
+  <image width="16" height="16" source="stairs_remove.png"/>
+ </tile>
+ <tile id="39">
+  <image width="16" height="16" source="../wood1.png"/>
+ </tile>
+ <tile id="40">
+  <image width="16" height="16" source="../wood2.png"/>
+ </tile>
+ <tile id="41">
+  <image width="16" height="16" source="../wood3.png"/>
+ </tile>
+ <tile id="42">
+  <image width="16" height="16" source="../pressure_plate1.png"/>
+ </tile>
+ <tile id="43">
+  <image width="16" height="16" source="../pillar.png"/>
+ </tile>
+ <tile id="44">
+  <image width="16" height="16" source="../coin.png"/>
+ </tile>
+ <tile id="45">
+  <image width="16" height="16" source="../firecanon_idle.png"/>
+ </tile>
+ <tile id="46">
+  <image width="10" height="10" source="double_jump.png"/>
+ </tile>
+ <tile id="47">
+  <image width="10" height="10" source="triple_jump.png"/>
+ </tile>
+ <tile id="48">
+  <image width="10" height="10" source="arrow_climb.png"/>
+ </tile>
+ <tile id="49">
+  <image width="16" height="24" source="catapult_.png"/>
+ </tile>
+</tileset>
